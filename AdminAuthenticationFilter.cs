5e0bcfcfd280a260a07b14f87319c5e117778ea7
mahmoudKFH <94610970+mahmoudKFH@users.noreply.github.com>
2022-02-06 09:35:47 +0300

    Add files via upload

---
+++ AdminAuthenticationFilter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Filters;
+
+namespace Online_Portal
+{
+    public class AdminAuthenticationFilter : ActionFilterAttribute, IAuthenticationFilter
+    {
+        public void OnAuthentication(AuthenticationContext filterContext)
+        {
+            //Check Session is Empty Then set as Result is HttpUnauthorizedResult 
+            if (string.IsNullOrEmpty(Convert.ToString(filterContext.HttpContext.Session["admin"])))
+            {
+                filterContext.Result = new HttpUnauthorizedResult();
+            }
+        }
+
+        public void OnAuthenticationChallenge(AuthenticationChallengeContext filterContext)
+        {
+            //We are checking Result is null or Result is HttpUnauthorizedResult 
+            // if yes then we are Redirect to Error View
+            if (filterContext.Result == null || filterContext.Result is HttpUnauthorizedResult)
+            {
+                filterContext.Result = new ViewResult
+                {
+                    ViewName = "Error"
+                };
+            }
+        }
+    }
+}
---
+++ IServices/ILaptopService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessAccessLayer.IServices
+{
+    public interface ILaptopService
+    {
+        DataSet GetProducts();
+        DataSet GetCategories();
+        int submitOrder(int Productid, string OrderNumber, int CustomerId);
+        int Login(string name, string Password);
+        bool Register(string Fname, string Lname, string email, string password);
+        string IsCustomerExists(string email);
+        DataTable GetProductyId(int id);
+        int AddProduct(decimal price,string productName, string color, string description, string screenSize, int categoryId, string brandName, string processor,bool available);
+        bool EditProduct(decimal price,string productName, string color, string description, string screenSize, int categoryId, string brandName, string processor,bool available,int productId);
+        void AddProductImage(int prodId, string imageUrl);
+    }
+}
